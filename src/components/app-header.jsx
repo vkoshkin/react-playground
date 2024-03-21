bc5d3a1a0011fce0243c1928937c31a98419433d
vkoshkin <mail@vkoshkin.com>
2024-03-21 19:58:45 +0300

    Add header items

--- src/components/app-header.jsx
+++ src/components/app-header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './app-header.module.css';
 
 class AppHeader extends React.Component {
@@ -7,7 +7,32 @@ class AppHeader extends React.Component {
     render() {
         return (
             <header className={styles.header}>
+                <nav className={styles.nav}>
+                    <div className="p-5 mt-4 mb-4">
+                        <span className={styles.anchor}>
+                            <BurgerIcon type="primary" />
+                            <span className="text text_type_main-default pl-2">Конструктор</span>
+                        </span>
+                    </div>
+
+                    <div className="p-5 mt-4 mb-4 ml-2">
+                        <span className={styles.anchor}>
+                            <ListIcon type="secondary" />
+                            <span className="text text_type_main-default text_color_inactive pl-2">Лента заказов</span>
+                        </span>
+                    </div>
+                </nav>
+
                 <Logo />
+
+                <div className={styles.personal}>
+                    <div className="p-5 mt-4 mb-4">
+                        <span className={styles.anchor}>
+                            <ProfileIcon type="secondary" />
+                            <span className="text text_type_main-default text_color_inactive pl-2">Личный кабинет</span>
+                        </span>
+                    </div>
+                </div>
             </header>
         );
     }
