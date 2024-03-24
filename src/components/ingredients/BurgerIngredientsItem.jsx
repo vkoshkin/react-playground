8489c0b3d38aaa9628877085b8f93b0570076c6b
vkoshkin <mail@vkoshkin.com>
2024-03-24 22:34:58 +0300

    Add counter display

--- src/components/ingredients/BurgerIngredientsItem.jsx
+++ src/components/ingredients/BurgerIngredientsItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './BurgerIngredientsItem.module.css';
 
@@ -8,16 +8,19 @@ class BurgerIngredientsItem extends React.Component {
     render() {
         return (
             <div className={styles.item}>
-                <img className="pl-4 pr-4 mb-1" src={this.props.item.image} alt={this.props.item.name} />
-                <div className={styles.item_price}>
-                    <span className="text text_type_digits-default">{this.props.item.price}</span>
-                    <CurrencyIcon type="primary" />
+                <div>
+                    <img className="pl-4 pr-4 mb-1" src={this.props.item.image} alt={this.props.item.name} />
+                    <div className={styles.item_price}>
+                        <span className="text text_type_digits-default">{this.props.item.price}</span>
+                        <CurrencyIcon type="primary" />
+                    </div>
+                    <div className={styles.item_description}>
+                        <p className="text text_type_main-default pt-1">
+                            {this.props.item.name}
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.item_description}>
-                    <p className="text text_type_main-default pt-1">
-                        {this.props.item.name}
-                    </p>
-                </div>
+                { this.props.count > 0 && <Counter count={this.props.count} size="default"/>}
             </div>
         );
     }
