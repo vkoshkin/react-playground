41db7399c3852039e862fd1b5184c26afae58600
vkoshkin <mail@vkoshkin.com>
2024-03-24 19:07:57 +0300

    Fix burger item styles

--- src/components/ingredients/BurgerIngredientsItem.jsx
+++ src/components/ingredients/BurgerIngredientsItem.jsx
@@ -8,14 +8,16 @@ class BurgerIngredientsItem extends React.Component {
     render() {
         return (
             <div className={styles.item}>
-                <img className="pl-4 pr-4 pb-1" src={this.props.item.image} alt={this.props.item.name} />
-                <div>
+                <img className="pl-4 pr-4 mb-1" src={this.props.item.image} alt={this.props.item.name} />
+                <div className={styles.item_price}>
                     <span className="text text_type_digits-default">{this.props.item.price}</span>
                     <CurrencyIcon type="primary" />
                 </div>
-                <p className="text text_type_main-default pt-1">
-                    {this.props.item.name}
-                </p>
+                <div className={styles.item_description}>
+                    <p className="text text_type_main-default pt-1">
+                        {this.props.item.name}
+                    </p>
+                </div>
             </div>
         );
     }
