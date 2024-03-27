384ea82adaa3a5c8734eebea1bce57fd96f01d5a
vkoshkin <mail@vkoshkin.com>
2024-03-27 14:12:50 +0300

    Fix constructor styling

--- src/components/burgerConstructor/BurgerConstructor.jsx
+++ src/components/burgerConstructor/BurgerConstructor.jsx
@@ -18,32 +18,29 @@ class BurgerConstructor extends React.Component {
 
         return (
             <div className={styles.constructor}>
-                <div className="mt-25">
-                    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                        {this.props.ingredients.top !== null && <ConstructorElement
-                            type="top"
-                            isLocked={true}
-                            text={this.props.ingredients.top.name}
-                            price={this.props.ingredients.bottom.price}
-                            thumbnail={this.props.ingredients.top.image}
-                            extraClass={styles.constructor_element}
-                        />}
-
-                        {this.props.ingredients.main.map(ingredient => <ConstructorElement
-                            text={ingredient.name}
-                            price={ingredient.price}
-                            thumbnail={ingredient.image}
-                            extraClass={styles.constructor_element}
-                        />)}
-                        {this.props.ingredients.bottom !== null && <ConstructorElement
-                            type="bottom"
-                            isLocked={true}
-                            text={this.props.ingredients.bottom.name}
-                            price={this.props.ingredients.bottom.price}
-                            thumbnail={this.props.ingredients.bottom.image}
-                            extraClass={styles.constructor_element}
-                        />}
-                    </div>
+                <div className={styles.constructor_list}>
+                    {this.props.ingredients.top !== null && <ConstructorElement
+                        type="top"
+                        isLocked={true}
+                        text={this.props.ingredients.top.name}
+                        price={this.props.ingredients.bottom.price}
+                        thumbnail={this.props.ingredients.top.image}
+                        extraClass={styles.constructor_element}
+                    />}
+                    {this.props.ingredients.main.map(ingredient => <ConstructorElement
+                        text={ingredient.name}
+                        price={ingredient.price}
+                        thumbnail={ingredient.image}
+                        extraClass={styles.constructor_element}
+                    />)}
+                    {this.props.ingredients.bottom !== null && <ConstructorElement
+                        type="bottom"
+                        isLocked={true}
+                        text={this.props.ingredients.bottom.name}
+                        price={this.props.ingredients.bottom.price}
+                        thumbnail={this.props.ingredients.bottom.image}
+                        extraClass={styles.constructor_element}
+                    />}
                 </div>
                 <div className={styles.constructor_footer}>
                     <div className={styles.constructor_price}>
