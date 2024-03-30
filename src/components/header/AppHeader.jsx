d5cf873dd962a03d6cec7571b2112c7226ec4677
vkoshkin <mail@vkoshkin.com>
2024-03-30 15:49:29 +0300

    Fix app header

--- src/components/header/AppHeader.jsx
+++ src/components/header/AppHeader.jsx
@@ -6,34 +6,42 @@ class AppHeader extends React.Component {
 
     render() {
         return (
-            <header className={styles.header}>
+            <div>
                 <nav className={styles.nav}>
-                    <div>
-                        <a className={styles.anchor} href="/">
-                            <BurgerIcon type="primary" />
-                            <span className="text text_type_main-default pl-2">Конструктор</span>
-                        </a>
+                    <div className={styles.menu_group}>
+                        <div>
+                            <a className={styles.anchor} href="/">
+                                <BurgerIcon type="primary" />
+                                <span className="text text_type_main-default pl-2">
+                                    Конструктор
+                                </span>
+                            </a>
+                        </div>
+
+                        <div className="ml-2">
+                            <a className={styles.anchor} href="/">
+                                <ListIcon type="secondary" />
+                                <span className="text text_type_main-default text_color_inactive pl-2">
+                                    Лента заказов
+                                </span>
+                            </a>
+                        </div>
                     </div>
 
-                    <div className="ml-2">
-                        <a className={styles.anchor} href="/">
-                            <ListIcon type="secondary" />
-                            <span className="text text_type_main-default text_color_inactive pl-2">Лента заказов</span>
-                        </a>
+                    <Logo />
+
+                    <div className={styles.personal}>
+                        <div>
+                            <a className={styles.anchor} href="/">
+                                <ProfileIcon type="secondary" />
+                                <span className="text text_type_main-default text_color_inactive pl-2">
+                                    Личный кабинет
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </nav>
-
-                <Logo />
-
-                <div className={styles.personal}>
-                    <div>
-                        <a className={styles.anchor} href="/">
-                            <ProfileIcon type="secondary" />
-                            <span className="text text_type_main-default text_color_inactive pl-2">Личный кабинет</span>
-                        </a>
-                    </div>
-                </div>
-            </header>
+            </div>
         );
     }
 }
