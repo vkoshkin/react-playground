60d8a9cbe6c73d0ccb13b4e554d3ae8490c98047
vkoshkin <mail@vkoshkin.com>
2024-03-30 23:26:52 +0300

    Add blank line

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
 import styles from './app-header.module.css';
 
 class AppHeader extends React.Component {
