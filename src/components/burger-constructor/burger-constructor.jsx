e4f3b02616b5efb0dfde13c4b6e1f51cf72cf094
vkoshkin <mail@vkoshkin.com>
2024-03-31 20:57:58 +0300

    Remove useless imports

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
--- src/components/burger-ingredients/burger-ingredients-item.jsx
+++ src/components/burger-ingredients/burger-ingredients-item.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
