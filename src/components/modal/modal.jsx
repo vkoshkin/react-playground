70ef810e81b3c037d8d2994413793dfbe31249b6
vkoshkin <mail@vkoshkin.com>
2024-05-24 21:38:06 +0300

    Add styling for modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,11 +1,18 @@
 import styles from './modal.module.css';
+import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function Modal(props) {
     const { onClose } = props;
     return (
         <div className={styles.modal}>
-            <div>I'm a modal dialog</div>
-            <button onClick={onClose}>Close</button>
+            <div className={styles.header}>
+                <span className={styles.header_text}>
+                    Заголовок
+                </span>
+                <div className={styles.close}>
+                    <CloseIcon type="primary" onClick={onClose}/>
+                </div>
+            </div>
         </div>
     );
 }
