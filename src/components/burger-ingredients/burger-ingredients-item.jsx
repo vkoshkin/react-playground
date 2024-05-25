691b4a51f133902629c6798bc455efae1d04a0cd
vkoshkin <mail@vkoshkin.com>
2024-05-25 21:12:38 +0300

    Add modal display for ingredient item

--- src/components/burger-ingredients/burger-ingredients-item.jsx
+++ src/components/burger-ingredients/burger-ingredients-item.jsx
@@ -1,15 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
+import ModalOverlay from '../modal/modal-overlay';
+import Modal from '../modal/modal';
 import styles from './burger-ingredients-item.module.css';
 import ingredientType from '../../utils/types';
 
 function BurgerIngredientsItem(props) {
+    const [showInfo, setShowInfo] = React.useState(false);
     const { ingredient, count, onAdd } = props;
+
+    const onIngredientImageClick = event => {
+        event.stopPropagation();
+        setShowInfo(true);
+    };
     return (
         <div className={styles.item} onClick={onAdd}>
             <div>
-                <img className="pl-4 pr-4 mb-1" src={ingredient.image} alt={ingredient.name} />
+                <img className="pl-4 pr-4 mb-1" 
+                    src={ingredient.image} 
+                    alt={ingredient.name} 
+                    onClick={onIngredientImageClick}/>
                 <div className={styles.item_price}>
                     <span className="text text_type_digits-default mr-1">{ingredient.price}</span>
                     <CurrencyIcon type="primary" />
@@ -21,6 +34,16 @@ function BurgerIngredientsItem(props) {
                 </div>
             </div>
             {count > 0 && <Counter count={count} size="default" />}
+            
+            {showInfo && ReactDOM.createPortal((
+                    <ModalOverlay onClose={() => setShowInfo(false)}>
+                        <Modal onClose={() => setShowInfo(false)}>
+                            
+                        </Modal>
+                    </ModalOverlay>
+                ),
+                document.body
+            )}
         </div>
     );
 }
