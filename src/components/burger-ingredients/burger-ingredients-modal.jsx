d497686bac1dcbe4728290df8bc189b5e8a2e8be
vkoshkin <mail@vkoshkin.com>
2024-05-25 22:49:58 +0300

    Fix ingredients modal

--- src/components/burger-ingredients/burger-ingredients-modal.jsx
+++ src/components/burger-ingredients/burger-ingredients-modal.jsx
@@ -11,21 +11,21 @@ function BurgerIngredientsModal(props) {
                     <img className={styles.image} src={ingredient.image_large} alt={ingredient.name} />
                     <span className={styles.name}>{ingredient.name}</span>
                     <div className={styles.nutritional_values_list}>
-                        <div className={styles.nutritional_values_item}>
-                            <span className={styles.nutritional_values_desc}>Калории,ккал</span>
-                            <span className={styles.nutritional_values_number}>{ingredient.calories}</span>
+                        <div className={styles.nutritional_value_item}>
+                            <span className={styles.nutritional_value_desc}>Калории, ккал</span>
+                            <span className={styles.nutritional_value_number}>{ingredient.calories}</span>
                         </div>
-                        <div className={styles.nutritional_values_item}>
-                            <span className={styles.nutritional_values_desc}>Белки, г</span>
-                            <span className={styles.nutritional_values_number}>{ingredient.proteins}</span>
+                        <div className={styles.nutritional_value_item}>
+                            <span className={styles.nutritional_value_desc}>Белки, г</span>
+                            <span className={styles.nutritional_value_number}>{ingredient.proteins}</span>
                         </div>
-                        <div className={styles.nutritional_values_item}>
-                            <span className={styles.nutritional_values_desc}>Жиры, г</span>
-                            <span className={styles.nutritional_values_number}>{ingredient.fat}</span>
+                        <div className={styles.nutritional_value_item}>
+                            <span className={styles.nutritional_value_desc}>Жиры, г</span>
+                            <span className={styles.nutritional_value_number}>{ingredient.fat}</span>
                         </div>
-                        <div className={styles.nutritional_values_item}>
-                            <span className={styles.nutritional_values_desc}>Углеводы, г</span>
-                            <span className={styles.nutritional_values_number}>{ingredient.carbohydrates}</span>
+                        <div className={styles.nutritional_value_item}>
+                            <span className={styles.nutritional_value_desc}>Углеводы, г</span>
+                            <span className={styles.nutritional_value_number}>{ingredient.carbohydrates}</span>
                         </div>
                     </div>
                 </div>
