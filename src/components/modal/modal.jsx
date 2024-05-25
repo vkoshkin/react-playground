abd01c8c0b56b5fa0309002cfb14407938891486
vkoshkin <mail@vkoshkin.com>
2024-05-25 21:54:49 +0300

    Add children to modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,7 +2,7 @@ import styles from './modal.module.css';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function Modal(props) {
-    const { header, onClose } = props;
+    const { header, children, onClose } = props;
     return (
         <div className={styles.modal}>
             <div className={styles.header}>
@@ -12,6 +12,9 @@ function Modal(props) {
                 <div className={styles.close}>
                     <CloseIcon type="primary" onClick={onClose}/>
                 </div>
+            </div>
+            <div className={styles.content}>
+                {children}
             </div>
         </div>
     );
