53fb15cb8195f3d15df3031c346ee14521faa8a7
vkoshkin <mail@vkoshkin.com>
2024-05-26 13:20:50 +0300

    Add order mark background

--- src/components/burger-constructor/burger-constructor-order.jsx
+++ src/components/burger-constructor/burger-constructor-order.jsx
@@ -3,6 +3,9 @@ import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-component
 import ModalOverlay from '../modal/modal-overlay';
 import Modal from '../modal/modal';
 import styles from './burger-constructor-order.module.css';
+import Order1 from '../../images/order-1.svg'
+import Order2 from '../../images/order-2.svg'
+import Order3 from '../../images/order-3.svg'
 
 function BurgerConstructorOrder(props) {
     const { onClose } = props;
@@ -13,7 +16,12 @@ function BurgerConstructorOrder(props) {
                     <span className={styles.order_id}>034536</span>
                     <span className={styles.order_id_desc}>идентификатор заказа</span>
                     <div className={styles.order_icon}>
-                        <CheckMarkIcon type="primary"/>
+                        <img src={Order1} alt="Заказ" className={styles.order_icon_segment}/>
+                        <img src={Order2} alt="Заказ" className={styles.order_icon_segment}/>
+                        <img src={Order3} alt="Заказ" className={styles.order_icon_segment}/>
+                        <div className={styles.order_icon_segment}>
+                            <CheckMarkIcon type="primary"   />
+                        </div>
                     </div>
                     <span className={styles.order_info}>Ваш заказ начали готовить</span>
                     <span className={styles.order_sub}>Дождитесь готовности на орбитальной станции</span>
