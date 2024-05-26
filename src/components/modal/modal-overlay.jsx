b898e124bb483990c237e8d7056e72413868581c
vkoshkin <mail@vkoshkin.com>
2024-05-26 14:18:56 +0300

    Remove blank lines

--- src/components/modal/modal-overlay.jsx
+++ src/components/modal/modal-overlay.jsx
@@ -5,20 +5,17 @@ import styles from './modal-overlay.module.css';
 
 function ModalOverlay(props) {
     const { children, onClose } = props;
-
 	const trackKeyDown = (e) => {
 	    if (e.key === "Escape") {
             onClose();
         }
 	};
-
     React.useEffect(()=>{
 	    document.addEventListener("keydown", trackKeyDown);
 	    return () => {
 		    document.removeEventListener("keydown", trackKeyDown);
 	    }
 	});
-
     return (
         <section className={styles.overlay} onClick={onClose}>
             {children}
