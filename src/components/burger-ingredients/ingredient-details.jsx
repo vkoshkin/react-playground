f5c2b692b745ed78330306263d54b00eb8b355a4
vkoshkin <mail@vkoshkin.com>
2024-05-27 13:49:06 +0300

    Move ModalOverlay inside Modal

--- src/components/burger-ingredients/ingredient-details.jsx
+++ src/components/burger-ingredients/ingredient-details.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 
-import ModalOverlay from '../modal/modal-overlay';
 import Modal from '../modal/modal';
 import styles from './ingredient-details.module.css';
 import ingredientType from '../../utils/types';
@@ -8,32 +7,30 @@ import ingredientType from '../../utils/types';
 function IngredientDetails(props) {
     const { ingredient, onClose } = props;
     return (
-        <ModalOverlay onClose={onClose}>
-            <Modal header="Детали ингредиента" onClose={onClose}>
-                <div className={styles.layout}>
-                    <img className={styles.image} src={ingredient.image_large} alt={ingredient.name} />
-                    <span className={styles.name}>{ingredient.name}</span>
-                    <div className={styles.nutritional_values_list}>
-                        <div className={styles.nutritional_value_item}>
-                            <span className={styles.nutritional_value_desc}>Калории, ккал</span>
-                            <span className={styles.nutritional_value_number}>{ingredient.calories}</span>
-                        </div>
-                        <div className={styles.nutritional_value_item}>
-                            <span className={styles.nutritional_value_desc}>Белки, г</span>
-                            <span className={styles.nutritional_value_number}>{ingredient.proteins}</span>
-                        </div>
-                        <div className={styles.nutritional_value_item}>
-                            <span className={styles.nutritional_value_desc}>Жиры, г</span>
-                            <span className={styles.nutritional_value_number}>{ingredient.fat}</span>
-                        </div>
-                        <div className={styles.nutritional_value_item}>
-                            <span className={styles.nutritional_value_desc}>Углеводы, г</span>
-                            <span className={styles.nutritional_value_number}>{ingredient.carbohydrates}</span>
-                        </div>
+        <Modal header="Детали ингредиента" onClose={onClose}>
+            <div className={styles.layout}>
+                <img className={styles.image} src={ingredient.image_large} alt={ingredient.name} />
+                <span className={styles.name}>{ingredient.name}</span>
+                <div className={styles.nutritional_values_list}>
+                    <div className={styles.nutritional_value_item}>
+                        <span className={styles.nutritional_value_desc}>Калории, ккал</span>
+                        <span className={styles.nutritional_value_number}>{ingredient.calories}</span>
+                    </div>
+                    <div className={styles.nutritional_value_item}>
+                        <span className={styles.nutritional_value_desc}>Белки, г</span>
+                        <span className={styles.nutritional_value_number}>{ingredient.proteins}</span>
+                    </div>
+                    <div className={styles.nutritional_value_item}>
+                        <span className={styles.nutritional_value_desc}>Жиры, г</span>
+                        <span className={styles.nutritional_value_number}>{ingredient.fat}</span>
+                    </div>
+                    <div className={styles.nutritional_value_item}>
+                        <span className={styles.nutritional_value_desc}>Углеводы, г</span>
+                        <span className={styles.nutritional_value_number}>{ingredient.carbohydrates}</span>
                     </div>
                 </div>
-            </Modal>
-        </ModalOverlay>
+            </div>
+        </Modal>
     );
 }
 
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,21 +2,24 @@ import PropTypes from 'prop-types';
 
 import styles from './modal.module.css';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import ModalOverlay from './modal-overlay';
 
 function Modal(props) {
     const { header, children, onClose } = props;
     return (
-        <div className={styles.modal} onClick={e => e.stopPropagation()}>
-            <div className={styles.header}>
-                <p className={styles.header_text}>{header}</p>
-                <div className={styles.close}>
-                    <CloseIcon type="primary" onClick={onClose} />
+        <ModalOverlay onClose={onClose}>
+            <div className={styles.modal} onClick={e => e.stopPropagation()}>
+                <div className={styles.header}>
+                    <p className={styles.header_text}>{header}</p>
+                    <div className={styles.close}>
+                        <CloseIcon type="primary" onClick={onClose} />
+                    </div>
+                </div>
+                <div>
+                    {children}
                 </div>
             </div>
-            <div>
-                {children}
-            </div>
-        </div>
+        </ModalOverlay>
     );
 }
 
