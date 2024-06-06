3324421ea7195dbcdce65221c6d974dbfd762296
vkoshkin <mail@vkoshkin.com>
2024-06-06 15:11:55 +0300

    Remove comments

--- src/services/app.js
+++ src/services/app.js
@@ -15,26 +15,6 @@ const appSlice = createSlice({
     name: "app",
     initialState,
     reducers: {
-        // todoAdded(state, action) {
-        //     const { id, text } = action.payload
-        //     state.todos.push({
-        //         id,
-        //         text,
-        //         completed: false
-        //     })
-        // },
-        // todoToggled(state, action) {
-        //     // Look for the specific nested object to update.
-        //     // In this case, `action.payload` is the default field in the action,
-        //     // and can hold the `id` value - no need for `action.id` separately
-        //     const matchingTodo = state.todos.find(todo => todo.id === action.payload)
-        //
-        //     if (matchingTodo) {
-        //         // Can directly "mutate" the nested object
-        //         matchingTodo.completed = !matchingTodo.completed
-        //     }
-        // }
-
         getIngredients(state, action) {
             state.dataRequest = true;
         },
@@ -59,9 +39,6 @@ const appSlice = createSlice({
     }
 });
 
-// `createSlice` automatically generated action creators with these names.
-// export them as named exports from this "slice" file
-// 
 export const { 
     getIngredients, 
     getIngredientsError, 
@@ -69,5 +46,4 @@ export const {
     addIngredient,
 } = appSlice.actions;
 
-// Export the slice reducer as the default export
 export default appSlice.reducer;
