3aef0021b2f28ff868d73765d92528bd63fa3cf1
vkoshkin <mail@vkoshkin.com>
2024-06-08 13:38:09 +0300

    Move request to another function

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,16 @@
-const BASE_URL = "https://norma.nomoreparties.space/api";
-
-const INGREDIENTS_API = `${BASE_URL}/ingredients`;
+import { request } from "../utils/requests";
 
 export function getIngredientsRequest() {
-    return fetch(INGREDIENTS_API)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(`Ошибка ${response.status}`);
-        });
+    return request("ingredients");
 }
 
-const ORDER_API = `${BASE_URL}/orders`;
-
 export function postOrderRequest(data) {
-    return fetch(ORDER_API, {
+    return request("orders", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         redirect: "follow",
         body: JSON.stringify({ ingredients: data }),
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(`Ошибка ${response.status}`);
-        });
+    });
 }
