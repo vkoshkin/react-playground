55d30054e39f1bc67dec10750726245a8d5fecb2
vkoshkin <mail@vkoshkin.com>
2024-06-14 20:15:07 +0300

    Add AppError as 404 page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,12 +3,14 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Home from "../../pages/home";
+import AppError from './app-error';
 
 function App() {
     return (
         <DndProvider backend={HTML5Backend}>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<AppError />} />
             </Routes>
         </DndProvider>
     );
