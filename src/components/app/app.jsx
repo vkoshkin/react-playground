0010dfd14d4d6a99a264d2b1361c17f5e02eb6d6
vkoshkin <mail@vkoshkin.com>
2024-06-18 20:18:26 +0300

    Add Profile page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,6 +8,7 @@ import Login from "../../pages/login";
 import Register from "../../pages/register";
 import ForgotPassword from "../../pages/forgot-password";
 import ResetPassword from "../../pages/reset-password";
+import Profile from "../../pages/profile";
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
+                <Route path="/profile" element={<Profile />} />
                 <Route path="*" element={<AppError />} />
             </Routes>
         </DndProvider>
