a17562ae66f11863258a6e342665603d1af15e2a
vkoshkin <mail@vkoshkin.com>
2024-06-25 18:26:34 +0300

    Add profile menu selection

--- src/pages/profile-menu.jsx
+++ src/pages/profile-menu.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import { logoutUser } from "../services/user";
 import styles from "./profile-menu.module.css";
@@ -9,14 +9,17 @@ function ProfileMenu() {
     const onExit = e => {
         dispatch(logoutUser());
     };
+    const location = useLocation();
+    const profileActive = location.pathname === "/profile";
+    const ordersActive = location.pathname === "/profile/orders";
     return (
         <div className={styles.sections}>
             <nav className={styles.nav}>
                 <Link to={""} className={styles.nav_link}>
-                    <p className={styles.nav_text_inactive}>Профиль</p>
+                    <p className={profileActive ? styles.nav_text : styles.nav_text_inactive}>Профиль</p>
                 </Link>
                 <Link to={"orders"} className={styles.nav_link}>
-                    <p className={styles.nav_text_inactive}>История заказов</p>
+                    <p className={ordersActive ? styles.nav_text : styles.nav_text_inactive}>История заказов</p>
                 </Link>
                 <div className={styles.nav_link} onClick={onExit}>
                     <p className={styles.nav_exit}>Выход</p>
