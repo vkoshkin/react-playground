996041e81fc41585bf0651ec9382f0c47c04c52a
vkoshkin <mail@vkoshkin.com>
2024-06-25 16:59:41 +0300

    Add profile save/cancel

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,27 +1,52 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Input, EmailInput, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Input, EmailInput, PasswordInput, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./profile.module.css";
 
 function Profile() {
     const { user } = useSelector(store => store.user);
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [changed, setChanged] = useState(false);
+    const dataChanged = (currName, currEmail) => {
+        setChanged(currName !== user.name || currEmail !== user.email);
+    };
     return (
         <div>
             <Input
                 placeholder="Имя"
-                value={user.name}
+                value={name}
+                onChange={e => { setName(e.target.value); dataChanged(e.target.value, email); }}
+                icon={"EditIcon"}
                 extraClass={styles.name}
             />
             <EmailInput
                 placeholder="Логин"
-                value={user.email}
+                value={email}
+                onChange={e => { setEmail(e.target.value); dataChanged(name, e.target.value); }}
+                icon={"EditIcon"}
+                isIcon={true}
                 extraClass={styles.email}
             />
             <PasswordInput
                 placeholder="Пароль"
                 value="**********"
+                onChange={() => { }}
+                disabled
+                icon={""}
                 extraClass={styles.password}
             />
+            {changed &&
+                <div className={styles.buttons}>
+                    <Button htmlType="button" type="primary" size="medium">
+                        Сохранить
+                    </Button>
+                    <Button htmlType="button" type="secondary" size="medium" extraClass={styles.cancel}>
+                        Отмена
+                    </Button>
+                </div>
+            }
         </div>
     );
 }
