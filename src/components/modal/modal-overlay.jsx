86ef1a863fb68f6333d39ac8c004710b2c5e4462
vkoshkin <mail@vkoshkin.com>
2024-06-26 20:18:35 +0300

    Move escape handler to modal

--- src/components/modal/modal-overlay.jsx
+++ src/components/modal/modal-overlay.jsx
@@ -1,21 +1,9 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 import styles from "./modal-overlay.module.css";
 
 function ModalOverlay(props) {
     const { children, onClose } = props;
-    React.useEffect(() => {
-        const trackKeyDown = (e) => {
-            if (e.key === "Escape") {
-                onClose();
-            }
-        };
-        document.addEventListener("keydown", trackKeyDown);
-        return () => {
-            document.removeEventListener("keydown", trackKeyDown);
-        }
-    }, [onClose]);
     return (
         <section className={styles.overlay} onClick={(e) => { e.stopPropagation(); onClose(); }}>
             {children}
