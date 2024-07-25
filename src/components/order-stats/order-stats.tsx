3972e98eda523931c772a72ae5fa402149fd80cd
vkoshkin <mail@vkoshkin.com>
2024-07-25 17:59:06 +0300

    Filled order stats templates

--- src/components/order-stats/order-stats.tsx
+++ src/components/order-stats/order-stats.tsx
@@ -5,6 +5,50 @@ import styles from "./order-stats.module.css";
 const OrderStats: FC = () => {
     return (
         <div>
+            <section className={styles.section}>
+                <div className={styles.rows}>
+                    <div className={styles.row}>
+                        <h2 className={styles.row_section_title}>
+                            Готовы:
+                        </h2>
+                        <ul className={styles.row_list}>
+                            <li className={styles.row_list_item}>034533</li>
+                            <li className={styles.row_list_item}>034532</li>
+                            <li className={styles.row_list_item}>034530</li>
+                            <li className={styles.row_list_item}>034527</li>
+                            <li className={styles.row_list_item}>034525</li>
+                        </ul>
+                    </div>
+                    <div className={styles.row}>
+                        <h2 className={styles.row_section_title}>
+                            В работе:
+                        </h2>
+                        <ul className={styles.row_list}>
+                            <li className={styles.row_list_item}>034538</li>
+                            <li className={styles.row_list_item}>034541</li>
+                            <li className={styles.row_list_item}>034542</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.section}>
+                <h2 className={styles.section_title}>
+                    Выполнено за всё время:
+                </h2>
+                <p className={styles.section_count}>
+                    28752
+                </p>
+            </section>
+
+            <section className={styles.section}>
+                <h2 className={styles.section_title}>
+                    Выполнено сегодня:
+                </h2>
+                <p className={styles.section_count}>
+                    138
+                </p>
+            </section>
         </div>
     );
 }
