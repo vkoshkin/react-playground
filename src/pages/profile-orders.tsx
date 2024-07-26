fb8d03e4a40c1fffc67bbb8c495bc22dc533bd6a
vkoshkin <mail@vkoshkin.com>
2024-07-26 12:58:48 +0300

    Add sample profile-orders

--- src/pages/profile-orders.tsx
+++ src/pages/profile-orders.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
 
+import OrderList from "../components/order-list/order-list";
+import styles from "./profile-orders.module.css";
+
 const ProfileOrders: FC = () => {
     return (
-        <div>
+        <div className={styles.orders}>
+            <OrderList/>
         </div>
     );
 }
