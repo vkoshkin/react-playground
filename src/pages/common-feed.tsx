f6fab212fa20a8534245e0a3f13847b83799ec92
vkoshkin <mail@vkoshkin.com>
2024-07-30 21:23:44 +0300

    Add connection to websocket on common feed

--- src/pages/common-feed.tsx
+++ src/pages/common-feed.tsx
@@ -1,10 +1,19 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
+import { useAppDispatch } from "../services/store";
+import { wsConnect, wsDisconnect } from "../services/common-feed";
 import OrderList from "../components/order-list/order-list";
 import OrderStats from "../components/order-stats/order-stats";
 import styles from "./common-feed.module.css";
 
 const CommonFeed: FC = () => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(wsConnect("wss://norma.nomoreparties.space/orders/all"));
+        return () => {
+            dispatch(wsDisconnect());
+        };
+    }, []);
     return (
         <div className={styles.feed}>
             <h1 className={styles.header}>Лента заказов</h1>
