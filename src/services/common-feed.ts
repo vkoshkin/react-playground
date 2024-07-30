c85b69b2c038bc9b6b35281b4d15f3e94bd1795b
vkoshkin <mail@vkoshkin.com>
2024-07-30 22:19:49 +0300

    Add message parsing from feed

--- src/services/common-feed.ts
+++ src/services/common-feed.ts
@@ -1,17 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-import { WebSocketStatus } from "../utils/websockets";
+import { FeedResult } from "./api";
 import { Order } from "./types";
+import { WebSocketStatus } from "../utils/websockets";
 
 type CommonFeedState = {
     status: WebSocketStatus;
     orders: Array<Order>;
+    ordersTotal: number;
+    ordersToday: number;
+    ready: Array<number>;
+    inProgress: Array<number>;
     error: boolean;
 };
 
 const initialState: CommonFeedState = {
     status: WebSocketStatus.OFFLINE,
     orders: [],
+    ordersTotal: 0,
+    ordersToday: 0,
+    ready: [],
+    inProgress: [],
     error: false,
 };
 
@@ -36,8 +45,32 @@ const slice = createSlice({
             console.log(`${state.status} ${action.payload}`);
             state.error = true;
         },
-        wsMessage: (state: CommonFeedState, action: PayloadAction<any>) => {
-            console.log(action.payload);
+        wsMessage: (state: CommonFeedState, action: PayloadAction<FeedResult>) => {
+            if (!action.payload.success) {
+                state.error = true;
+                return;
+            }
+
+            const ready: Array<number> = [];
+            const inProgress: Array<number> = [];
+            const set = new Set<string>();
+
+            for (const order of action.payload.orders) {
+                set.add(order.status);
+                if (order.status === "done") {
+                    ready.push(order.number);
+                } else {
+                    inProgress.push(order.number);
+                }
+            }
+            console.log(set);
+
+            state.orders = action.payload.orders;
+            state.ordersTotal = action.payload.total;
+            state.ordersToday = action.payload.totalToday;
+            state.ready = ready;
+            state.inProgress = inProgress;
+            state.error = false;
         },
         wsDisconnect: (state: CommonFeedState) => {
             state.status = WebSocketStatus.OFFLINE;
