5f2ff0b64d06626f0de319097451ec69099fdef6
vkoshkin <mail@vkoshkin.com>
2024-08-01 18:32:29 +0300

    Add template for profile-orders

--- src/pages/profile-orders.tsx
+++ src/pages/profile-orders.tsx
@@ -1,12 +1,33 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
+import { useAppDispatch, useTypedSelector } from "../services/store";
+import { WebSocketStatus, WS_PROFILE_URL } from "../utils/websockets";
+import { profileWsConnect, profileWsDisconnect } from "../services/profile-feed";
 import styles from "./profile-orders.module.css";
 
 const ProfileOrders: FC = () => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(profileWsConnect(WS_PROFILE_URL));
+        return () => {
+            dispatch(profileWsDisconnect());
+        };
+    }, []);
+
+    const { status, orders } = useTypedSelector(store => store.profileFeed);
 
     return (
         <div className={styles.orders}>
-            
+            {(status !== WebSocketStatus.ONLINE || orders.length === 0) &&
+                <div className={styles.loading}>
+                    <div>
+                        <div className="loader"></div>
+                    </div>
+                </div>
+            }
+            {status === WebSocketStatus.ONLINE &&
+                <></>
+            }
         </div>
     );
 }
