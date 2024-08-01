c83c1d8166ee3a4d440de4425170dd2ed068f525
vkoshkin <mail@vkoshkin.com>
2024-08-01 16:19:30 +0300

    Fix common feed display

--- src/components/order-stats/order-stats.tsx
+++ src/components/order-stats/order-stats.tsx
@@ -6,31 +6,41 @@ import styles from "./order-stats.module.css";
 const OrderStats: FC = () => {
     const { ordersTotal, ordersToday, ready, inProgress } = useTypedSelector(store => store.commonFeed);
     return (
-        <div>
-            <section className={styles.section}>
-                <div className={styles.columns}>
-                    <div className={styles.column}>
-                        <h2 className={styles.column_section_title}>
-                            Готовы:
-                        </h2>
+        <div className={styles.stats}>
+            <div className={styles.columns}>
+                <section className={styles.column}>
+                    <h2 className={styles.section_title}>
+                        Готовы:
+                    </h2>
+                    <div className={styles.column_scroll}>
                         <ul className={styles.column_list}>
-                            {ready.map(orderNumber => 
-                                <li key={orderNumber} className={styles.column_list_item_finished}>{orderNumber}</li>
+                            {ready.map(orderNumber =>
+                                <li
+                                    key={orderNumber}
+                                    className={styles.column_list_item_finished}
+                                >
+                                    {orderNumber}
+                                </li>
                             )}
                         </ul>
                     </div>
-                    <div className={styles.column}>
-                        <h2 className={styles.column_section_title}>
-                            В работе:
-                        </h2>
-                        <ul className={styles.column_list}>
-                            {inProgress.map(orderNumber => 
-                                <li key={orderNumber} className={styles.column_list_item}>{orderNumber}</li>
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            </section>
+                </section>
+                <section className={styles.column}>
+                    <h2 className={styles.section_title}>
+                        В работе:
+                    </h2>
+                    <ul className={styles.column_list}>
+                        {inProgress.map(orderNumber =>
+                            <li
+                                key={orderNumber}
+                                className={styles.column_list_item}
+                            >
+                                {orderNumber}
+                            </li>
+                        )}
+                    </ul>
+                </section>
+            </div>
 
             <section className={styles.section}>
                 <h2 className={styles.section_title}>
