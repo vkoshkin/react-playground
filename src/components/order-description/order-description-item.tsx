cf3f42a57081c452d92d52866623cf56e2733fca
vkoshkin <mail@vkoshkin.com>
2024-08-02 15:00:47 +0300

    Add order-description-item impl

--- src/components/order-description/order-description-item.tsx
+++ src/components/order-description/order-description-item.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 
-import styles from "./order-description-item.module.css";
 import { Ingredient } from "../../services/types";
+import IngredientImage from "../ingredient-image/ingredient-image";
+import Price from "../price/price";
+import styles from "./order-description-item.module.css";
 
 interface OrderDescriptionItemProps {
     readonly ingredient: Ingredient;
@@ -10,6 +12,16 @@ interface OrderDescriptionItemProps {
 const OrderDescriptionItem: FC<OrderDescriptionItemProps> = ({ ingredient }) => {
     return (
         <div className={styles.item}>
+            <IngredientImage
+                image={ingredient.image}
+                altName={ingredient.name}
+            />
+            <div className={styles.item_text}>
+                <p className={styles.text}>
+                    {ingredient.name}
+                </p>
+            </div>
+            <Price price={ingredient.price}/>
         </div>
     );
 };
